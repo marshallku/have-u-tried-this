623c578c789baeda14d266239366ebcdf6c3f0e0
구영표 <marshallkelice@gmail.com>
2021-12-15 15:37:51 +0900

    Update PostItem to display location if possible

--- frontend/src/js/router/List.js
+++ frontend/src/js/router/List.js
@@ -10,6 +10,9 @@ export default function List(data) {
         title: article.title,
         thumbnail: article.photo,
         slug: article.id,
+        location: article.wide_addr
+          ? `${article.wide_addr} ${article.local_addr}`
+          : null,
         likes: article.likes,
       }),
     ),
