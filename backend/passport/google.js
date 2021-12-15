8c73c4014859e058d8a63f60bfe9cd8907cfa083
Jisung <jasdepw@gmail.com>
2021-12-16 02:52:59 +0900

    Try integrate passport with google

--- backend/passport/google.js
+++ backend/passport/google.js
@@ -1,49 +1,25 @@
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require("mongoose");
-const User = require("../models/schemas/User");
-require('dotenv').config()
+import passport from "passport";
+import mongoose from "mongoose";
+import UserSchema from "../models/schemas/User";
 
-const config = {
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: `http://localhost:3000/oauth/google/callback`,
+let GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+require("dotenv").config();
+
+const User = mongoose.model("User", UserSchema);
+
+const passportConfig = {
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/callback",
 };
 
-async function findOrCreateUser({ email }) {
-  const user = await User.findOne({
-    email,
-  });
-
-  if (user) {
-    console.log(user);
-    return user;
-  }
-
-  const created = await User.create({
-    googleId,
-    email,
-    firstName,
-    lastName,
-    profileImage,
-  });
-
-  return created;
-}
-
-module.exports = new GoogleStrategy(config, async (accessToken, refreshToken, profile, done) => {
-  const { email } = profile._json;
-
-  try {
-    const user = await findOrCreateUser({ email })
-    done(null, {
-      googleId: profile.id,
-      email: profile.emails[0].value,
-      firstName: profile.name.givenName,
-      lastName: profile.name.familyName,
-      image: profile.photos[0].value,
-    });
-  } catch (e) {
-    done(e, null);
-  }
-});
+passport.use(
+  new GoogleStrategy(
+    passportConfig,
+    (accessToken, refreshToken, profile, done) => {
+      User.findOrCreate({ googleId: profile.id }, (err, user) =>
+        done(err, user),
+      );
+    },
+  ),
+);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,70 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable consistent-return */
 /* eslint-disable import/extensions */
 import { Router } from "express";
+import dotenv from "dotenv";
+import passport from "passport";
 import mongoose from "mongoose";
+import session from "express-session";
+import { v4 as uuidv4 } from "uuid";
+import passportGoogleOAuth from "passport-google-oauth2";
+import MongoDBSession from "connect-mongodb-session";
 import asyncHandler from "../utils/async-handler.js";
 import UserSchema from "../models/schemas/User.js";
 
+dotenv.config();
+
 const router = Router();
+const GoogleStrategy = passportGoogleOAuth.Strategy;
+const User = mongoose.model("User", UserSchema);
+const passportConfig = {
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/callback",
+};
+
+const MongoDBStore = MongoDBSession(session);
+const sessionStore = new MongoDBStore({
+  uri: "mongodb://localhost:27017/session",
+  collection: "mySessions",
+});
+
+router.use(
+  session({
+    secret: "secret-key",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+// passport 초기화 및 session 연결
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.use(
+  new GoogleStrategy(
+    passportConfig,
+    (accessToken, refreshToken, profile, done) => {
+      User.findOrCreate({ googleId: profile.id }, (err, user) =>
+        done(err, user),
+      );
+    },
+  ),
+);
+
+// Google OAuth
+router.get("/google", (req, res) => {
+  console.log("TEST");
+  passport.authenticate("google", { scope: ["email", "profile"] });
+});
 
 router.get(
-  "/callback/google",
-  asyncHandler(async (req, res) => {
-    res.json({ status: "ok" });
-  }),
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    res.redirect("/");
+  },
 );
 
 router.get(
@@ -18,8 +72,8 @@ router.get(
   asyncHandler(async (req, res) => {
     const User = mongoose.model("User", UserSchema);
     const newUser = new User({
-      googleId: "googleId",
-      email: "email",
+      googleId: uuidv4(),
+      email: `${uuidv4()}@gmail.com`,
       firstName: "firstName",
       lastName: "lastName",
       profileImage: "profileImage",
@@ -30,5 +84,11 @@ router.get(
     res.json({ created: newUser });
   }),
 );
+
+// logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
 
 export default router;
