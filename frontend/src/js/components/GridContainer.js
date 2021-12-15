0e3c550dab2baad54e3d6a141df9746ae130515c
구영표 <marshallkelice@gmail.com>
2021-12-15 15:28:54 +0900

    Implement GridContainer

---
+++ frontend/src/js/components/GridContainer.js
@@ -0,0 +1,9 @@
+import "../../css/GridContainer.css";
+
+export default function GridContainer() {
+  const container = document.createElement("div");
+
+  container.classList.add("grid-container");
+
+  return container;
+}
