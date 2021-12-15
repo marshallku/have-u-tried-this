d513b734504f3701f6b045c2335651a9e122374a
Jisung <jasdepw@gmail.com>
2021-12-16 02:01:17 +0900

    Fix formattings

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,21 +2,21 @@
 import { Router } from "express";
 import mongoose from "mongoose";
 import asyncHandler from "../utils/async-handler.js";
-import UserSchema from '../models/schemas/User.js'
+import UserSchema from "../models/schemas/User.js";
 
 const router = Router();
 
 router.get(
   "/callback/google",
   asyncHandler(async (req, res) => {
-    res.json({ 'status': 'ok' });
+    res.json({ status: "ok" });
   }),
 );
 
 router.get(
   "/test",
   asyncHandler(async (req, res) => {
-    const User = mongoose.model('User', UserSchema);
+    const User = mongoose.model("User", UserSchema);
     const newUser = new User({
       googleId: "googleId",
       email: "email",
@@ -25,9 +25,9 @@ router.get(
       profileImage: "profileImage",
       lastLoginAt: new Date(),
       isActive: true,
-    })
+    });
     newUser.save();
-    res.json({ 'created': newUser });
+    res.json({ created: newUser });
   }),
 );
 
