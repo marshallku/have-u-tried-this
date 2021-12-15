8677fe7e1d5b9bde89691fcf98bd42d06246a3b7
구영표 <marshallkelice@gmail.com>
2021-12-15 02:17:45 +0000

    Update dummy.js

---
+++ frontend/src/js/api/index.js
@@ -0,0 +1,6 @@
+import fetchLocationData from "./location";
+import fetchPostData from "./post";
+import fetchListData from "./list";
+import fetchUserData from "./user";
+
+export { fetchLocationData, fetchPostData, fetchListData, fetchUserData };
