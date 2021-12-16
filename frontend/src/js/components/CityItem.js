367d6316674b702ceb8ce40d8ce9cb94efa136dc
구영표 <marshallkelice@gmail.com>
2021-12-16 08:01:04 +0000

    Index page

---
+++ frontend/src/js/components/CityItem.js
@@ -0,0 +1,32 @@
+import "../../css/CityItem.css";
+
+export default function CityItem({ wide, local, thumbnail }) {
+  const article = document.createElement("article");
+  const anchor = document.createElement("a");
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  const header = document.createElement("header");
+  const wideLocation = document.createElement("h2");
+  const localLocation = document.createElement("h3");
+
+  // Article
+  article.classList.add("city-item");
+  anchor.href = `/location/${wide}/${local}`;
+
+  // Thumbnail
+  figure.classList.add("city-item__thumbnail");
+  img.src = thumbnail;
+  figure.append(img);
+
+  // Header
+  header.classList.add("city-item__header");
+  wideLocation.innerText = wide;
+  localLocation.innerText = local;
+  header.append(wideLocation, localLocation);
+
+  // Append
+  anchor.append(figure, header);
+  article.append(anchor);
+
+  return article;
+}
--- frontend/src/js/pages/ListPage.js
+++ frontend/src/js/pages/ListPage.js
@@ -1,11 +1,28 @@
-import List from "../components/List";
+import PostItem from "../components/PostItem";
+import GridContainer from "../components/GridContainer";
 import { fetchPostListData } from "../api/dummy";
 
 export default function FrontPage() {
   const div = document.createElement("div");
 
   fetchPostListData().then((data) => {
-    div.append(List(data));
+    const container = GridContainer();
+
+    data.forEach((article) =>
+      container.append(
+        PostItem({
+          title: article.title,
+          thumbnail: article.photo,
+          slug: article.id,
+          location: article.wideAddr
+            ? `${article.wideAddr} ${article.localAddr}`
+            : null,
+          likes: article.likes,
+        }),
+      ),
+    );
+
+    div.append(container);
   });
 
   return div;
