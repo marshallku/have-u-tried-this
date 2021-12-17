389d4e59763689610de8f1bbb481dcde883e5f79
구영표 <marshallkelice@gmail.com>
2021-12-17 17:59:54 +0900

    Update sign in page design

--- frontend/src/js/pages/SignInPage.js
+++ frontend/src/js/pages/SignInPage.js
@@ -2,19 +2,30 @@ import { addClickEvent } from "../router";
 import "../../css/SignInPage.css";
 
 export default function SignInPage() {
-  const div = document.createElement("div");
+  const container = document.createElement("div");
   const logoWrap = document.createElement("div");
   const logo = document.createElement("img");
+  const signInWrap = document.createElement("div");
+  const signInTitle = document.createElement("h2");
+  const signInInfo = document.createElement("p");
   const signInButton = document.createElement("button");
   const googleIcon = document.createElement("img");
   const signInText = document.createElement("span");
 
-  div.classList.add("sign-in");
+  container.classList.add("sign-in");
 
   // Logo
   logoWrap.classList.add("sign-in__logo");
   logo.src = "/static/images/logo.svg";
   logoWrap.append(logo);
+
+  // Sign In Container
+  // Title
+  signInWrap.classList.add("sign-in__container");
+  signInTitle.classList.add("sign-in__title");
+  signInInfo.classList.add("sign-in__info");
+  signInTitle.innerText = "로그인";
+  signInInfo.innerText = "구글로 간편하게 로그인 하고 맛식 저장, 공유 하세요!";
 
   // Button
   signInButton.classList.add("sign-in__button");
@@ -23,8 +34,10 @@ export default function SignInPage() {
   signInButton.append(googleIcon, signInText);
   addClickEvent(signInButton, "/user");
 
-  // Append
-  div.append(logoWrap, signInButton);
+  signInWrap.append(signInTitle, signInInfo, signInButton);
 
-  return div;
+  // Append
+  container.append(logoWrap, signInWrap);
+
+  return container;
 }
