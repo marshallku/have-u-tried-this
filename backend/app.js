5366e827d485ae5c5f86491ceafcb486b6a6a467
Jisung <jasdepw@gmail.com>
2021-12-17 23:32:05 +0900

    Fix favicon.ico 404 issues

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-console */
 /* eslint-disable import/extensions */
 import express, { json } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import path from "path";
+import favicon from "serve-favicon";
 import postRouter from "./routes/posts.js";
 import locationRouter from "./routes/locations.js";
 import authRouter from "./routes/auth.js";
@@ -14,6 +17,10 @@ const port = process.env.PORT || 3000;
 
 app.use(json());
 
+// favicon
+const __dirname = path.resolve();
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+
 // mongo DB & mongoose
 const mongoUri = process.env.MONGO_DB_URI;
 mongoose.connect(`${mongoUri}/test`);
@@ -23,6 +30,11 @@ app.use("/api/locations", locationRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/auth", authRouter);
 
+// server listen
+app.listen(port, () => {
+  // console.log("Server start");
+});
+
 // Error handler
 // 404 error
 app.use((req, res, next) => {
@@ -30,10 +42,5 @@ app.use((req, res, next) => {
 });
 app.use((err, req, res) => {
   console.error(err.stack);
-  res.status(500).json({ message: err });
-});
-
-// server listen
-app.listen(port, () => {
-  // console.log("Server start");
+  res.status(500).send({ message: err });
 });
