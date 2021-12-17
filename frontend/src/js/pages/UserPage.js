d1ed4c635af43d3fb6f35fc7e5392cf64f6a67fb
구영표 <marshallkelice@gmail.com>
2021-12-17 13:14:10 +0000

    Update user page

--- frontend/src/js/pages/UserPage.js
+++ frontend/src/js/pages/UserPage.js
@@ -2,98 +2,68 @@ import UserPageInfo from "../components/UserPageInfo";
 import UserPagePost from "../components/UserPagePost";
 import UserPageComment from "../components/UserPageComment";
 import UserPageLike from "../components/UserPageLike";
-import { fetchUserData } from "../api/dummy";
+import { customRouter } from "../router";
 import "../../css/userPage.css";
 
-const Main = document.createElement("main");
-const Title = document.createElement("h1");
-
-function renderTitle(string) {
-  Title.innerText = string;
-}
-
-function resetPage() {
-  Main.innerHTML = "";
-}
-
-function renderPage(text, component) {
-  resetPage();
-  renderTitle(text);
-  Main.append(component);
-}
-
-function Drawer() {
+function Drawer(userPageRouter) {
   const nav = document.createElement("nav");
-  const profileWrap = document.createElement("div");
-  const profile = document.createElement("img");
-  const Button = ({ text, className, render }) => {
+  const Button = ({ text, path }) => {
     const li = document.createElement("li");
     const a = document.createElement("a");
 
-    li.classList.add("nav__item", `nav__${className}`);
-
-    a.href = `/settings/profile/${className}`;
+    a.classList.add("nav__item");
     a.innerText = text;
-    a.addEventListener("click", (event) => {
-      event.preventDefault();
-      renderPage(text, render());
-    });
+    userPageRouter.addNavigator(a, path);
 
     li.append(a);
 
     return li;
   };
   const ul = document.createElement("ul");
-  const info = Button({
-    text: "내 정보",
-    className: "info",
-    render: UserPageInfo,
-  });
   const post = Button({
     text: "작성 글",
-    className: "post",
-    render: UserPagePost,
+    path: "/",
   });
   const comment = Button({
     text: "작성 댓글",
-    className: "comment",
-    render: UserPageComment,
+    path: "/comment",
   });
   const liked = Button({
     text: "추천한 글",
-    className: "liked",
-    render: UserPageLike,
+    path: "/liked",
   });
 
   // Nav
   nav.classList.add("nav");
 
-  // Profile
-  profileWrap.classList.add("nav__photo");
-  fetchUserData().then((response) => {
-    profile.src = response.profile;
-    profileWrap.append(profile);
-  });
-
   // List items
   ul.classList.add("nav__items");
-  ul.append(info, post, comment, liked);
+  ul.append(post, comment, liked);
 
   // Append
-  nav.append(profileWrap);
   nav.append(ul);
 
   return nav;
 }
 
 export default function UserPage() {
+  const userPageRouter = customRouter();
+  userPageRouter.base = "user";
+  userPageRouter.routes = {
+    "/": UserPagePost,
+    "/comment": UserPageComment,
+    "/liked": UserPageLike,
+  };
+
   const fragment = document.createDocumentFragment();
-  const drawer = Drawer();
+  const drawer = Drawer(userPageRouter);
+  const main = document.createElement("main");
 
-  Main.classList.add("settings");
-  Title.classList.add("settings__title");
+  userPageRouter.baseElement = main;
+  userPageRouter.initialize(window.location.pathname.replace("user", ""));
 
-  fragment.append(drawer, Title, Main);
+  main.classList.add("settings");
+  fragment.append(drawer, main);
 
   return fragment;
 }
--- frontend/src/js/router/index.js
+++ frontend/src/js/router/index.js
@@ -8,6 +8,44 @@ export function route() {
   renderPage(path);
 }
 
+export function customRouter() {
+  const router = {
+    base: "",
+    baseElement: null,
+    routes: {},
+    navigators: [],
+    update(path) {
+      const uriPath = `/${this.base}${path === "/" ? "" : path}`;
+
+      window.scrollTo(0, 0);
+      window.history.pushState("", document.title, uriPath);
+
+      this.navigators.forEach((x) => x.classList.remove("highlight"));
+      this.navigators
+        .filter((x) => x.pathname === uriPath)
+        .forEach((x) => x.classList.add("highlight"));
+
+      if (this.routes[path]) {
+        this.baseElement.innerHTML = "";
+        this.baseElement.append(this.routes[path]());
+      }
+    },
+    addNavigator(elt, path) {
+      elt.setAttribute("href", `/${this.base}${path === "/" ? "" : path}`);
+      elt.addEventListener("click", (event) => {
+        event.preventDefault();
+        this.update(path);
+      });
+      this.navigators.push(elt);
+    },
+    initialize(path) {
+      this.update(path);
+    },
+  };
+
+  return router;
+}
+
 export function updatePath(path) {
   window.scrollTo(0, 0);
   window.history.pushState("", document.title, path);
