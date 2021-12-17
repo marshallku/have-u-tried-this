f94f27ef1befbc397cb7b4041cd02bdf7f8a6872
Jisung <jasdepw@gmail.com>
2021-12-18 04:04:40 +0900

    Remove pw field in models

--- backend/models/schemas/User.js
+++ backend/models/schemas/User.js
@@ -13,7 +13,6 @@ const UserSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   profilePhoto: String,
-  password: String,
   source: { type: String, required: [true, "source not specified"] },
   lastVisited: { type: Date, default: new Date() },
 });
