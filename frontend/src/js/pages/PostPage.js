942691296895c300948ba0d7ddf355fc39670b68
구영표 <marshallkelice@gmail.com>
2021-12-18 03:43:04 +0000

    Update post page

--- frontend/src/js/pages/PostPage.js
+++ frontend/src/js/pages/PostPage.js
@@ -4,26 +4,56 @@ import { fetchPostData } from "../api/dummy";
 import "../../css/PostDetails.css";
 
 function renderPostDetails(data) {
+  const {
+    title,
+    author,
+    content,
+    pictures,
+    likes,
+    location,
+    createdAt,
+    updatedAt,
+  } = data;
   const frag = document.createDocumentFragment();
-  const carousel = Carousel(data.pictures);
-  const smallContainer = document.createElement("section");
-  const postInfo = document.createElement("div");
-  const likes = LikesCount(data.likes);
-  const title = document.createElement("h2");
-  const author = document.createElement("div");
+  const carousel = Carousel(pictures);
+  const smallContainer = document.createElement("div");
+  const header = document.createElement("header");
+  const locationWrap = document.createElement("div");
+  const locationIcon = document.createElement("i");
+  const locationText = document.createElement("span");
+  const dateWrap = document.createElement("div");
+  const dateIcon = document.createElement("i");
+  const dateText = document.createElement("span");
+  const postDate = new Date(`${updatedAt || createdAt}`);
+  const buttonsWrap = document.createElement("div");
+  const likesElt = LikesCount(likes);
+  const titleElt = document.createElement("h2");
+  const authorElt = document.createElement("div");
   const authorAvatar = document.createElement("img");
   const authorName = document.createElement("span");
-  const desc = document.createElement("p");
+  const descElt = document.createElement("p");
   const editing = {
     status: false,
     title: "",
     desc: "",
   };
 
-  // Post Info
-  likes.classList.add("details__rate");
-  postInfo.classList.add("details__info");
-  postInfo.append(likes);
+  // Location
+  locationWrap.classList.add("details__location");
+  locationIcon.classList.add("icon-location_on");
+  locationText.innerText = `${location.wideAddr} ${location.localAddr}`;
+  locationWrap.append(locationIcon, locationText);
+
+  // Date
+  dateWrap.classList.add("details__date");
+  dateIcon.classList.add("icon-calendar_today");
+  dateText.innerText = postDate.toLocaleDateString("ko-KR");
+  dateWrap.append(dateIcon, dateText);
+
+  // Buttons
+  likesElt.classList.add("details__rate");
+  buttonsWrap.classList.add("details__buttons");
+  buttonsWrap.append(likesElt);
   if (data.isAuthor) {
     const edit = document.createElement("button");
 
@@ -31,72 +61,76 @@ function renderPostDetails(data) {
     edit.addEventListener("click", () => {
       const nextStatus = !editing.status;
 
-      title.contentEditable = nextStatus;
-      desc.contentEditable = nextStatus;
+      titleElt.contentEditable = nextStatus;
+      descElt.contentEditable = nextStatus;
 
       if (editing.status) {
         // End editing
         edit.className = "icon-create";
         // Remove Attributes
-        title.removeAttribute("role");
-        title.classList.remove("details__title--editing");
-        desc.removeAttribute("role");
-        desc.classList.remove("details__desc--editing");
+        titleElt.removeAttribute("role");
+        titleElt.classList.remove("details__title--editing");
+        descElt.removeAttribute("role");
+        descElt.classList.remove("details__desc--editing");
 
         // Fill Text if empty
-        if (!title.innerText) title.innerText = editing.title;
-        if (!desc.innerText) desc.innerText = editing.desc;
+        if (!titleElt.innerText) titleElt.innerText = editing.title;
+        if (!descElt.innerText) descElt.innerText = editing.desc;
 
         // Validate text
-        if (title.innerText.includes("\n"))
-          title.innerText = title.innerText.replaceAll("\n", " ");
+        if (titleElt.innerText.includes("\n"))
+          titleElt.innerText = titleElt.innerText.replaceAll("\n", " ");
 
         // TODO: Update post with api
       } else {
         // Start editing
         edit.className = "icon-save";
-        editing.title = title.innerText;
-        editing.desc = desc.innerText;
-        title.setAttribute("role", "textbox");
-        title.classList.add("details__title--editing");
-        desc.setAttribute("role", "textbox");
-        desc.classList.add("details__desc--editing");
-        title.focus();
+        editing.title = titleElt.innerText;
+        editing.desc = descElt.innerText;
+        titleElt.setAttribute("role", "textbox");
+        titleElt.classList.add("details__title--editing");
+        descElt.setAttribute("role", "textbox");
+        descElt.classList.add("details__desc--editing");
+        titleElt.focus();
       }
 
       editing.status = nextStatus;
     });
-    postInfo.append(edit);
+    buttonsWrap.append(edit);
   }
 
   // Title
-  title.classList.add("details__title");
-  title.innerText = data.title;
+  titleElt.classList.add("details__title");
+  titleElt.innerText = title;
 
   // Author
-  author.classList.add("details__author");
-  authorAvatar.src = data.author.profile;
-  authorName.innerText = `by ${data.author.nickname}`;
-  author.append(authorAvatar, authorName);
+  authorElt.classList.add("details__author");
+  authorAvatar.src = author.profile;
+  authorName.innerText = `by ${author.nickname}`;
+  authorElt.append(authorAvatar, authorName);
+
+  // Append above to header
+  header.classList.add("details__header");
+  header.append(dateWrap, locationWrap, buttonsWrap, titleElt, authorElt);
 
   // desc
-  desc.classList.add("details__desc");
-  desc.innerText = data.content;
+  descElt.classList.add("details__desc");
+  descElt.innerText = content;
 
   // Container
   smallContainer.classList.add("small-container", "details");
-  smallContainer.append(postInfo, title, author, desc);
+  smallContainer.append(header, descElt);
   frag.append(carousel, smallContainer);
 
   return frag;
 }
 
 export default function PostDetails() {
-  const div = document.createElement("div");
+  const article = document.createElement("article");
 
   fetchPostData().then((data) => {
-    div.append(renderPostDetails(data));
+    article.append(renderPostDetails(data));
   });
 
-  return div;
+  return article;
 }
