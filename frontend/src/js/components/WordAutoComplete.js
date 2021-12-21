0cf720db20952b1a8ef68d23467bed0dfed92d8c
안영우 <ywtechit@gmail.com>
2021-12-21 16:00:51 +0000

    Refactor upload page

--- frontend/src/js/components/WordAutoComplete.js
+++ frontend/src/js/components/WordAutoComplete.js
@@ -2,17 +2,18 @@ import el from "../utils/dom";
 import { fetchAddressData } from "../api";
 
 export default function WordAutoComplete() {
-  const dataList = el("datalist", {});
+  const dataList = el("datalist", { id: "address" });
   const fragment = document.createDocumentFragment();
 
-  fetchAddressData().then((response) => {
-    Object.entries(response).forEach(([wideAddr, localAddr]) => {
-      localAddr.forEach((city) => {
-        fragment.append(el("option", { value: `${wideAddr} ${city}` }));
+  new Promise((resolve) => {
+    fetchAddressData().then((response) => {
+      Object.entries(response).forEach(([wideAddr, localAddr]) => {
+        localAddr.forEach((city) => {
+          fragment.append(el("option", { value: `${wideAddr} ${city}` }));
+        });
       });
     });
-  });
-
-  dataList.append(fragment);
+    resolve(fragment);
+  }).then((fragment) => dataList.append(fragment));
   return dataList;
 }
