efdde7dbae9efff489a3615c82c3724c85353c36
구영표 <marshallkelice@gmail.com>
2021-12-21 14:10:18 +0900

    Fix imagesLoaded to return promise

--- frontend/src/js/utils/imagesLoaded.js
+++ frontend/src/js/utils/imagesLoaded.js
@@ -1,18 +1,18 @@
 export default function imagesLoaded(elements, func) {
-  const images = elements
-    .map((element) => [...element.querySelectorAll("img")])
-    .filter((x) => x.length)
-    .reduce((acc, cur) => acc.concat(cur), []);
-  const imagesLength = images.length;
-  let loaded = 0;
-  const tryCallback = () => {
-    loaded += 1;
-    if (imagesLength === loaded) func();
-  };
+  return new Promise((res) => {
+    const images = elements
+      .map((element) => [...element.querySelectorAll("img")])
+      .filter((x) => x.length)
+      .reduce((acc, cur) => acc.concat(cur), []);
+    const imagesLength = images.length;
+    let loaded = 0;
+    const tryCallback = () => {
+      loaded += 1;
+      if (imagesLength === loaded) res(func());
+    };
 
-  images.forEach((img) => {
-    img.addEventListener("load", tryCallback, { once: true });
+    images.forEach((img) => {
+      img.addEventListener("load", tryCallback, { once: true });
+    });
   });
-
-  return elements;
 }
