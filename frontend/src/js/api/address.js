c8c9fbf18de6b18e335b665a6a18363b88739928
안영우 <ywtechit@gmail.com>
2021-12-22 07:54:41 +0000

    Add error handling

--- frontend/src/js/api/address.js
+++ frontend/src/js/api/address.js
@@ -1,24 +1,30 @@
 import instance from "./instance";
+import createInstance from "../utils/fetcher";
 
 export async function fetchAddressData() {
   return instance.get("/locations/all");
 }
 
+// eslint-disable-next-line consistent-return
 export async function fetchAddressAPI(longitude, latitude) {
-  const kakaoApiUri = `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${longitude}&y=${latitude}`;
+  const URL = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json";
+  const resource = `?x=${longitude}&y=${latitude}`;
   const request = {
     method: "GET",
     headers: {
-      Authorization: `KakaoAK 4be5731b9445ebbbc4acf16e9d6f0588`,
+      Authorization: "KakaoAK 4be5731b9445ebbbc4acf16e9d6f0588",
     },
   };
 
-  // TODO: fetch가 정상적으로 수행되지 않았을 때 예외 처리하는 함수 만들기
-  const response = await fetch(kakaoApiUri, request);
-  const [legalDivision] = await response.json();
-  // eslint-disable-next-line camelcase
-  const { region_1depth_name, region_2depth_name } = legalDivision;
+  const kakaoInstance = createInstance({ baseUrl: URL });
+  const data = await kakaoInstance.post(resource, request);
 
-  // eslint-disable-next-line camelcase
-  return { wideAddr: region_1depth_name, localAddr: region_2depth_name };
+  const [legalDivision] = data.documents;
+  const { region_1depth_name: wideAddr, region_2depth_name: localAddr } =
+    legalDivision;
+
+  return {
+    wideAddr,
+    localAddr,
+  };
 }
