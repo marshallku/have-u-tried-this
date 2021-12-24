25d149da47dad94e752ac3c4851cc58f19f6fa9b
구영표 <marshallkelice@gmail.com>
2021-12-24 20:24:26 +0900

    Add type writer component

---
+++ frontend/src/js/components/Typewriter.js
@@ -0,0 +1,36 @@
+import el from "../utils/dom";
+
+export default function Typewriter(texts, speed = 250) {
+  let i = 0;
+  let j = 0;
+
+  const element = el("div", {});
+
+  const txtNum = () => (j === texts.length - 1 ? 0 : j + 1);
+
+  const type = () => {
+    if (i < texts[j].length) {
+      element.innerText += texts[j].charAt(i);
+      i += 1;
+      setTimeout(type, speed);
+    } else {
+      // eslint-disable-next-line no-use-before-define
+      remove();
+    }
+  };
+
+  const remove = () => {
+    if (i >= 0) {
+      element.innerText = element.innerText.slice(0, i);
+      i -= 1;
+      setTimeout(remove, speed);
+    } else {
+      type();
+      j = txtNum();
+    }
+  };
+
+  type();
+
+  return element;
+}
