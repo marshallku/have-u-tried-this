e842c23adc8d7ccd85cbd2a7770661127d349bdb
sinf <wjdtp93@gmail.com>
2021-12-24 15:45:18 +0900

    Fix req body limits - 50mb

--- backend/loader/expressLoader.js
+++ backend/loader/expressLoader.js
@@ -18,8 +18,8 @@ export default (app) => {
   callPassport();
 
   app.use(cors());
-  app.use(express.json({ limit: "30mb" }));
-  app.use(express.urlencoded({ extended: false, limit: "30mb" }));
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({ extended: false, limit: "50mb" }));
   app.use(cookieParser());
 
   app.use(passport.initialize());
