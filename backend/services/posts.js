166bdc499f1044e1d3d85ee33d907e0962e2fa64
sinf <wjdtp93@gmail.com>
2021-12-25 01:13:32 +0900

    Fix delete post

--- backend/services/posts.js
+++ backend/services/posts.js
@@ -192,11 +192,11 @@ export async function deletePost(postId, authorId) {
     if (isExist.author.toString() !== authorId.toString()) {
       throw new Error("권한이 없습니다.");
     }
+    await Comment.deleteMany({ post: postId });
+    await Bookmark.deleteMany({ post: postId });
     const post = await Post.findByIdAndDelete(postId);
     // 사진 삭제, DB에서는 삭제 되는지 확인 필요
     // 관련 코멘트 북마크 모두 삭제
-    await Comment.deleteMany({ post: postId });
-    await Bookmark.deleteMany({ post: postId });
     const { photos, location } = post;
     photos.forEach((photo) => {
       fs.unlinkSync(path.join(process.env.UPLOAD_PATH, photo.filename));
