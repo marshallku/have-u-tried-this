3eb7a365b3b067009c0aa58736845112c9454163
sinf <wjdtp93@gmail.com>
2021-12-24 15:36:14 +0900

    Fix req body limits - 30mb

--- backend/loader/expressLoader.js
+++ backend/loader/expressLoader.js
@@ -18,7 +18,7 @@ export default (app) => {
   callPassport();
 
   app.use(cors());
-  app.use(express.json());
+  app.use(express.json({ limit: "30mb" }));
   app.use(express.urlencoded({ extended: false, limit: "30mb" }));
   app.use(cookieParser());
 
