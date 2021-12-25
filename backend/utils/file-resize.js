d6d46396a38e58c449b44db7116a48357eb0b8d1
sinf <wjdtp93@gmail.com>
2021-12-25 12:51:57 +0900

    Fix file resize

--- backend/utils/file-resize.js
+++ backend/utils/file-resize.js
@@ -6,15 +6,11 @@ import { writeFileSync } from "fs";
 dotenv.config();
 
 export default async function resizeFile(files) {
-  files.forEach((file) => {
-    const filePath = path.join(process.env.UPLOAD_PATH, file.filename);
-
-    sharp(filePath)
-      .resize({ width: 1080 })
-      .withMetadata()
-      .toBuffer((err1, buffer) => {
-        if (err1) throw err1;
-        writeFileSync(filePath, buffer);
-      });
-  });
+  await Promise.all(
+    files.map(async (file) => {
+      const filePath = path.join(process.env.UPLOAD_PATH, file.filename);
+      const buff = await sharp(filePath).resize({ width: 1080 }).toBuffer();
+      writeFileSync(filePath, buff);
+    }),
+  );
 }
